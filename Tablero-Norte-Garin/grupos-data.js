016645590a28226a01b673b685eb4defe23a6fd4
s1m0n4ss <horasalgado08@gmail.com>
2026-03-06 13:23:52 -0300

    Crear grupos-data.js

---
+++ Tablero-Norte-Garin/grupos-data.js
@@ -0,0 +1,114 @@
+// grupos-data.js
+// Datos de los Grupos de Predicación
+// Congregación Garín
+
+window.GRUPOS_DATA = [
+
+{
+  id: 1,
+  nombre: "Grupo 1",
+  publicadores: 19,
+  superintendente: "Segovia León",
+  auxiliar: "Daniel Albis",
+  integrantes: [
+    "Becerra Nely",
+    "Benítez Elida",
+    "Cha Elena",
+    "Céspedes Angela",
+    "Collado Daniel",
+    "Di Marino Alicia",
+    "Ducoli Gustavo",
+    "Ducoli Teodora",
+    "Harlos Morelia",
+    "Iniazzoto Antonio",
+    "Meza Sebastián",
+    "Quaranta Daniel",
+    "Quaranta Graciela",
+    "Ramírez Julio",
+    "Sarraute Diego",
+    "Sarraute Micaela",
+    "Segovia Mirta"
+  ]
+},
+
+{
+  id: 2,
+  nombre: "Grupo 2",
+  publicadores: 18,
+  superintendente: "Salgado Horacio",
+  auxiliar: "Mario Segovia",
+  integrantes: [
+    "Casas Carmen",
+    "Coria Elías",
+    "Coria Sara",
+    "Feril Manuel",
+    "Humancondor Danter",
+    "Humancondor Martina",
+    "Laurente Paulina",
+    "Leiva Guillermina",
+    "Palacios Bibiana",
+    "Palavecino Héctor",
+    "Palavecino Roxana",
+    "Rojas Rosa",
+    "Salgado Sabrina",
+    "Sánchez Alicia",
+    "Talarico Doris",
+    "Villantoy Fany"
+  ]
+},
+
+{
+  id: 3,
+  nombre: "Grupo 3",
+  publicadores: 20,
+  superintendente: "Casco Elvio",
+  auxiliar: "Cortez Darío",
+  integrantes: [
+    "Cansino León",
+    "Cansino Lidia",
+    "Cansino Silvia",
+    "Casco Mónica",
+    "Flores Calixto",
+    "Flores Elvira",
+    "Godoy Cintia",
+    "Leal Cecilia",
+    "Gómez Miriam",
+    "Miño Guillermo",
+    "Miño Mabel",
+    "Natalé Tamara",
+    "Segovia Cintia",
+    "Segovia Cristian",
+    "Sánchez Juan Carlos",
+    "Zúñiga Mabel",
+    "Zúñiga Pablo",
+    "Zúñiga Sofía"
+  ]
+},
+
+{
+  id: 4,
+  nombre: "Grupo 4",
+  publicadores: 18,
+  superintendente: "Gutiérrez Adolfo",
+  auxiliar: "Lucas Segovia",
+  integrantes: [
+    "Araos Florencia",
+    "Araoz Francisco",
+    "Araoz Susana",
+    "Barrera Norma",
+    "Cansino José",
+    "Cansino Lola",
+    "Dubs Abigail",
+    "Durán Benjamín",
+    "Durán Lucinda",
+    "Durán Samuel",
+    "Ferrari Caetano",
+    "Gualmes Rocio",
+    "Gutiérrez Andrea",
+    "Palladino Federico",
+    "Pereyra Melany",
+    "Scotti Norma"
+  ]
+}
+
+];
