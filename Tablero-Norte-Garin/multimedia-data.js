b9d464984f82f9dc142d63d880a1ca44e8462250
s1m0n4ss <horasalgado08@gmail.com>
2026-03-20 23:05:18 -0300

    Crear multimedia-data.js

---
+++ Tablero-Norte-Garin/multimedia-data.js
@@ -0,0 +1,46 @@
+window.MULTIMEDIA_DATA = [
+  {
+    key: "2026-03-02",
+    label: "02–08 Mar",
+    start: "2026-03-02",
+    end: "2026-03-08",
+    audio_video: ["Darío Cortez", "Caetano Ferrari"],
+    microfonistas: ["Samuel Durán", "Sebastián Meza"],
+    plataforma: ["Daniel Albiz"],
+    entrada: ["A confirmar"],
+    auditorio: ["A confirmar"]
+  },
+  {
+    key: "2026-03-09",
+    label: "09–15 Mar",
+    start: "2026-03-09",
+    end: "2026-03-15",
+    audio_video: ["Mario Segovia", "Horacio Salgado"],
+    microfonistas: ["Daniel Quaranta", "Elías Coria"],
+    plataforma: ["Diego Sarraute"],
+    entrada: ["Pablo Zuñiga"],
+    auditorio: ["Adolfo Gutierrez"]
+  },
+  {
+    key: "2026-03-16",
+    label: "16–22 Mar",
+    start: "2026-03-16",
+    end: "2026-03-22",
+    audio_video: ["Mario Segovia", "Caetano Ferrari"],
+    microfonistas: ["Elvio Casco", "Elias Coria"],
+    plataforma: ["Diego Sarraute"],
+    entrada: ["Horacio Salgado"],
+    auditorio: ["Francisco Araoz"]
+  },
+  {
+    key: "2026-03-23",
+    label: "23–30 Mar",
+    start: "2026-03-23",
+    end: "2026-03-30",
+    audio_video: ["Dario Cortez", "Horacio Salgado"],
+    microfonistas: ["Daniel Quaranta", "Samuel Duran"],
+    plataforma: ["Caetano Ferrari"],
+    entrada: ["Cristin Segovia"],
+    auditorio: ["León Segovia"]
+  }
+];
