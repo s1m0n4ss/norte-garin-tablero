93ba3b2a74381b45b3526ef267ed3e010e6a9afd
s1m0n4ss <horasalgado08@gmail.com>
2026-03-21 00:13:02 -0300

    Crear lectores-data.js

---
+++ Tablero-Norte-Garin/lectores-data.js
@@ -0,0 +1,37 @@
+window.LECTORES_DATA = [
+  {
+    monthLabel: "Marzo 2026",
+    year: 2026,
+    monthIdx: 2,
+    atalaya: [
+      { date: "01", special: true, title: "ASAMBLEA DE CIRCUITO", dayOverride: "Dom", typeOverride: "special", typeLabelOverride: "Evento" },
+      { date: "08", name: "ADOLFO GUTIERREZ" },
+      { date: "15", name: "LEON SEGOVIA" },
+      { date: "22", name: "LUCAS SEGOVIA" },
+      { date: "29", name: "DARIO CORTEZ" }
+    ],
+    biblico: [
+      { date: "05", name: "DIEGO SARRAUTE" },
+      { date: "12", name: "HORACIO SALGADO" },
+      { date: "19", name: "DANIEL ALBIS" },
+      { date: "26", name: "DARIO CORTEZ" }
+    ]
+  },
+  {
+    monthLabel: "Abril 2026",
+    year: 2026,
+    monthIdx: 3,
+    atalaya: [
+      { date: "05", name: "MARIO SEGOVIA" },
+      { date: "12", name: "HORACIO SALGADO" },
+      { date: "19", name: "ADOLFO GUTIERREZ" },
+      { date: "26", name: "DANIEL ALBIS" }
+    ],
+    biblico: [
+      { date: "09", name: "LUCAS SEGOVIA" },
+      { date: "16", name: "ADOLFO GUTIERREZ" },
+      { date: "23", name: "HORACIO SALGADO" },
+      { date: "30", name: "DIEGO SARRAUTE" }
+    ]
+  }
+];
