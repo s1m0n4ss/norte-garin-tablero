ff1b530b55e6823e754e940aa856fc5dc0c6d664
jpzuniga27-del <j.p.zuniga27@gmail.com>
2026-05-09 18:51:57 -0300

    Update multimedia-data.js

--- Tablero-Norte-Garin/multimedia-data.js
+++ Tablero-Norte-Garin/multimedia-data.js
@@ -1,43 +1,32 @@
 window.MULTIMEDIA_DATA = [
   {
-    key: "2026-04-13",
-    label: "13–19 Abr",
-    start: "2026-04-13",
-    end: "2026-04-19",
-    audio_video: ["Lucas Segovia", "Cristian Segovia"],
-    microfonistas: ["Samuel Duran", "Elias Coria"],
-    plataforma: ["Diego Sarraute"],
-    entrada: ["Leòn Segovia"],
-    auditorio: ["Daniel Quaranta"]
-  },
-  {
-    key: "2026-04-20",
-    label: "20–26 Abr",
-    start: "2026-04-20",
-    end: "2026-04-26",
-    audio_video: ["Mario Segovia", "Dario Cortez"],
-    microfonistas: ["Daniel Quaranta", "Daniel Albis"],
-    plataforma: ["Caetano Ferrari"],
-    entrada: ["Adolfo Gutierrez"],
-    auditorio: ["Diego Sarraute"]
-  },
-  {
-    key: "2026-04-27",
-    label: "27–03 May",
-    start: "2026-04-24",
-    end: "2026-05-03",
-    audio_video: ["Caetano Ferrari", "Pablo Zuñiga"],
-    microfonistas: ["Samuel Duran", "Elias Coria"],
-    plataforma: ["Diego Sarraute"],
-    entrada: ["Daniel Albis"],
-    auditorio: ["Francisco Araoz"]
-  },
-  {
     key: "2026-05-11",
     label: "11–17 May",
     start: "2026-05-11",
     end: "2026-05-17",
     audio_video: ["Horacio Salgado", "Pablo Zuñiga"],
+    microfonistas: ["Samuel Duran", "Elias Coria"],
+    plataforma: ["Diego Sarraute"],
+    entrada: ["A confirmar"],
+    auditorio: ["A confirmar"]
+  },
+  {
+    key: "2026-05-18",
+    label: "18–24 May",
+    start: "2026-05-18",
+    end: "2026-05-24",
+    audio_video: ["Mario Segovia", "Caetano Ferrari"],
+    microfonistas: ["Daniel Quaranta", "Daniel Albis"],
+    plataforma: ["Horacio Salgado"],
+    entrada: ["A confirmar"],
+    auditorio: ["A confirmar"]
+  },
+  {
+    key: "2026-05-25",
+    label: "25–31 May",
+    start: "2026-05-25",
+    end: "2026-05-31",
+    audio_video: ["Lucas Segovia", "Mario Segovia"],
     microfonistas: ["Samuel Duran", "Elias Coria"],
     plataforma: ["Diego Sarraute"],
     entrada: ["A confirmar"],
