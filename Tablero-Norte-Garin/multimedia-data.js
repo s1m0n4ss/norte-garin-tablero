eb9735e839844c3e365d19ff07300b9a41a6b309
jpzuniga27-del <j.p.zuniga27@gmail.com>
2026-08-05 17:57:55 -0300

    Update multimedia-data.js

--- Tablero-Norte-Garin/multimedia-data.js
+++ Tablero-Norte-Garin/multimedia-data.js
@@ -1,103 +1,36 @@
 window.MULTIMEDIA_DATA = [
 {
-    key: "2026-06-01",
-    label: "01–07 Jun",
-    start: "2026-06-01",
-    end: "2026-06-07",
-    audio_video: ["Horacio Salgado", "Pablo Zuñiga"],
-    microfonistas: ["Daniel Albis", "Elias Coria"],
+    key: "2026-08-03",
+    label: "03–09 Ago",
+    start: "2026-08-03",
+    end: "2026-08-09",
+    audio_video: ["Lucas Segovia", "Pablo Zuñiga"],
+    microfonistas: ["Samuel Duran", "Manuel Feril"],
     plataforma: ["Diego Sarraute"],
     entrada: ["Francisco Araoz"],
-    auditorio: ["Daniel Quaranta"]
+    auditorio: ["León Segovia"]
   },
  {
-    key: "2026-06-08",
-    label: "08–14 Jun",
-    start: "2026-06-08",
-    end: "2026-06-14",
-    audio_video: ["Lucas Segovia", "Dario Cortez"],
-    microfonistas: ["Daniel Quaranta", "Samuel Duran"],
+    key: "2026-08-10",
+    label: "10–16 Ago",
+    start: "2026-08-10",
+    end: "2026-08-16",
+    audio_video: ["Sebastian Brascetta", "Caetano Ferrari"],
+    microfonistas: ["Daniel Albis", "Elias Coria"],
     plataforma: ["Horacio Salgado"],
+    entrada: ["Adolfo Gutierrez"],
+    auditorio: ["Pablo Zuñiga"]
+  }, 
+  {
+    key: "2026-08-24",
+    label: "24–30 Ago",
+    start: "2026-08-24",
+    end: "2026-08-30",
+    audio_video: ["Lucas Dos Santos", "Sebastian Brascetta"],
+    microfonistas: ["Tomas Duran", "Samuel Duran"],
+    plataforma: ["Caetano Ferrari"],
     entrada: ["Elias Coria"],
     auditorio: ["Diego Sarraute"]
   }, 
-  {
-    key: "2026-06-15",
-    label: "15–21 Jun",
-    start: "2026-06-15",
-    end: "2026-06-21",
-    audio_video: ["Caetano Ferrari", "Pablo Zuñiga"],
-    microfonistas: ["Elias Coria", "Samuel Duran"],
-    plataforma: ["Diego Sarraute"],
-    entrada: ["Dario Cortez"],
-    auditorio: ["Juan Carlos Sanchez"]
-  }, 
-    {
-    key: "2026-06-22",
-    label: "22–28 Jun",
-    start: "2026-06-22",
-    end: "2026-06-28",
-    audio_video: ["Dario Cortez", "Lucas Segovia"],
-    microfonistas: ["Daniel Quaranta", "Daniel Albiz"],
-    plataforma: ["Horacio Salgado"],
-    entrada: ["Pablo Zuñiga"],
-    auditorio: ["Adolfo Gutierrez"]
-  }, 
-  {
-    key: "2026-06-29",
-    label: "29–05 Jul",
-    start: "2026-06-29",
-    end: "2026-07-05",
-    audio_video: ["Horacio Salgado", "Pablo Zuñiga"],
-    microfonistas: ["Samuel Duran", "Elias Coria"],
-    plataforma: ["Diego Sarraute"],
-    entrada: ["Elvio Casco"],
-    auditorio: ["Daniel Quaranta"]
-  }, 
-    {
-    key: "2026-07-06",
-    label: "06–12 Jul",
-    start: "2026-07-06",
-    end: "2026-07-12",
-    audio_video: ["Lucas Dos Santos", "Cristian Segovia"],
-    microfonistas: ["Daniel Albis", "Elias Coria"],
-    plataforma: ["Diego Sarraute"],
-    entrada: ["Adolfo Gutierrez"],
-    auditorio: ["Dario Cortez"]
-  }, 
-{
-    key: "2026-07-13",
-    label: "13–19 Jul",
-    start: "2026-07-13",
-    end: "2026-07-19",
-    audio_video: ["Lucas Segovia", "Caetano Ferrari"],
-    microfonistas: ["Benjamin Duran", "Samuel Duran"],
-    plataforma: ["Horacio Salgado"],
-    entrada: ["Francisco Araoz"],
-    auditorio: ["León Segovia"]
-  }, 
-
-{
-    key: "2026-07-20",
-    label: "20–26 Jul",
-    start: "2026-07-20",
-    end: "2026-07-26",
-    audio_video: ["Dario Cortez", "Pablo Zuñiga"],
-    microfonistas: ["Daniel Albis", "Tomas Duran"],
-    plataforma: ["Benjamin Duran"],
-    entrada: ["Elias Coria"],
-    auditorio: ["Horacio Salgado"]
-  },
-
-{
-    key: "2026-07-27",
-    label: "27–02 Jul",
-    start: "2026-07-27",
-    end: "2026-08-02",
-    audio_video: ["Lucas Dos Santos", "Lucas Segovia"],
-    microfonistas: ["Daniel Quaranta", "Elias Coria"],
-    plataforma: ["Horacio Salgado"],
-    entrada: ["Juan Carlos Sanchez"],
-    auditorio: ["Daniel Albis"]
-  },    
+ 
 ];
